23c740247feee38292a3f56b1554c79eacf6df30
AysenurKaraslan <aysenurkaraslann@gmail.com>
2015-08-08 19:25:19 +0300

    Git Ogreniyoruzzz

---
+++ gun1/Lyk1.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agnes
+ * Date: 08.08.2015
+ * Time: 15:32
+ */
+
+/*TYPE CONVERSION  */
+
+/*
+ $a = 22 ;
+ settype($a,"string");
+ echo gettype($a);
+*/
+//---------------------------------
+/* TYPE CONVERSION KISAYOLU */
+/*
+$a =(string) 22 ;
+echo gettype($a);
+*/
+
+///----------------------------------
+
+/*operatorler
+*/
+$sayi1 = 5;
+$sayi2 =3;
+//echo  $sayi1 + $sayi2;
+//echo  $sayi1 - $sayi2;
+//echo  $sayi1 * $sayi2;
+//echo  $sayi1 / $sayi2;
+//echo ceil( $sayi1 / $sayi2 );  //ceil yukari yuvarlar.
+//echo floor( $sayi1 / $sayi2 );  //floor asagi yuvarlar.
+//echo  $sayi1 % $sayi2;
+
+/*
+$sayi1++; //arttirdi
+echo $sayi1; // hoop yazdii 6 oldu
+*/
+
+/*
+//diyelim ki
+echo $sayi1++;  //dersek yazar sonra arttirir ekranda hala 5 olur
+*/
+echo $$$sayi2;
+
+
+
+
+?>
